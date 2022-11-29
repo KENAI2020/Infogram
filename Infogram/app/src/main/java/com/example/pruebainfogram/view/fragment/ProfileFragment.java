df85c4c971e36777eaa22023c7b3d7dbf630f07d
Kenai2020 <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>
2022-11-29 15:20:27 -0400

    Se sube CircleImageView y SimpleViewBehavior

--- Infogram/app/src/main/java/com/example/pruebainfogram/view/fragment/ProfileFragment.java
+++ Infogram/app/src/main/java/com/example/pruebainfogram/view/fragment/ProfileFragment.java
@@ -2,13 +2,21 @@ package com.example.pruebainfogram.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.pruebainfogram.R;
+import com.example.pruebainfogram.adapter.CardViewAdapter;
+import com.example.pruebainfogram.model.Image;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +69,49 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //toolbar
+        showToolBar("", false, view);
+
+        //recyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_profile);
+
+        //Layout manager
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        //el adapter
+        CardViewAdapter cardViewAdapter = new CardViewAdapter(buidImages(), R.layout.cardview_image, getActivity());
+        recyclerView.setAdapter(cardViewAdapter);
+        return view;
+    }
+
+    public void showToolBar(String titulo, boolean botonSubir, View view){
+        Toolbar toolbar = view.findViewById(R.id.toolbar_profile);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(titulo);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+
+    //creamos la lista de imagenes
+    public ArrayList<Image> buidImages(){
+        ArrayList<Image> images = new ArrayList<>();
+
+        images.add(new Image("https://9968c6ef49dc043599a5-e151928c3d69a5a4a2d07a8bf3efa90a.ssl.cf2.rackcdn.com/1130199.jpg", "Carlos Valdivia","2 dias", "1 me gusta"));
+        images.add(new Image("https://landingwebimg.s3.amazonaws.com/uploads/landing_page_product_variant/image/194595/image-upload1606687503.jpeg", "Luis Peres","4 dias", "3 me gusta"));
+        images.add(new Image("https://www.laregion.bo/wp-content/uploads/2019/07/coroico-parapente.jpeg", "Mauricio Quiroz","1 dias", "5 me gusta"));
+        images.add(new Image("https://www.ibolivia.org/wp-content/uploads/2019/08/laguna-colorada-flamencos.jpg", "Danilo Salazar","6 dias", "7 me gusta"));
+        images.add(new Image("https://www.eldiario.net/portal/wp-content/uploads/2021/10/pol-42-15.jpg", "Daniel Torres","9 dias", "2 me gusta"));
+        images.add(new Image("https://miaventuraviajando.com/wp-content/uploads/2018/10/samaipata-mi-aventura-viajando-30.jpg", "Lorena Herrera","4 dias", "4 me gusta"));
+        images.add(new Image("https://www.laregion.bo/wp-content/uploads/2015/10/cristo.jpg", "Maria Guzman","7 dias", "6 me gusta"));
+        images.add(new Image("https://lh5.googleusercontent.com/-CINb2hHdBHc/VMfJjASBQDI/AAAAAAAAC0I/23gSET6uSaw/s640/blogger-image-979580039.jpg", "Rolando Morales","5 dias", "8 me gusta"));
+        images.add(new Image("https://laicismo.org/wp-content/uploads/2017/02/Monumento-Virgen-de-Socavon-en-Oruro-Bolivia.jpg", "Fernando Castro","3 dias", "2 me gusta"));
+        images.add(new Image("https://tipsparatuviaje.com/wp-content/uploads/2019/07/salar-de-uyuni.jpg", "Laura Rojas","1 dias", "3 me gusta"));
+
+
+        return images;
     }
 }
